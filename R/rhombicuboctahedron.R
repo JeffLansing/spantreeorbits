c028f6b849a293f9703ee5bed4d4c6d722860654
JeffLansing <lansing2357@gmail.com>
2025-02-14 14:28:01 -0800

    Added rhombicuboctahedron

--- R/rhombicuboctahedron.R
+++ R/rhombicuboctahedron.R
@@ -10,38 +10,37 @@ NULL
 #' @importFrom stringr str_replace
 NULL
 
-#' Title Get a Cuboctrahedron
-#' Translate the cuboctahedron coordinates from the dmcooey library into
+#' Title Get a Rhombicuboctahedron
+#' Translate the rhombicuboctahedron coordinates from the dmcooey library into
 #' R code
 #'
-#' @return a list describing a cuboctahedron
+#' @return a list describing a rhombicuboctahedron
 #' @export
 #'
 #' @examples
-#' co <- get_cuboctahedron()
-#' stopifnot(nrow(co$vertices) == 12)
+#' co <- get_rhombicuboctahedron()
+#' stopifnot(nrow(co$vertices) == 24)
 #'
 #' @include sto_helpers.R
-get_cuboctahedron <- function() {
+get_rhombicuboctahedron <- function() {
 
-  data <- readLines("http://dmccooey.com/polyhedra/Cuboctahedron.txt")
-  code <- c()
-  for(i in 3:3) code <- c(code, data[i] %>% str_replace("=.+=", '=') %>%
-                            str_c(';'))
+  data <- readLines("http://dmccooey.com/polyhedra/Rhombicuboctahedron.txt")
+
+  code <- data[3] %>% str_replace("=.+=", '=') %>% str_c(';')
   tmp <- c()
-  for(i in 5:16) tmp <- c(tmp, data[i] %>% str_replace("V.+= ", "c"))
+  for(i in 5:28) tmp <- c(tmp, data[i] %>% str_replace("V.+= ", "c"))
   code <- c(code, tmp %>% str_c(collapse = ", ") %>% str_c("verts <- rbind(", ., ");"))
   tmp <- c()
-  for(i in 19:24) tmp <- c(tmp, data[i] %>% str_replace("\\{", "c(") %>%
+  for(i in 31:48) tmp <- c(tmp, data[i] %>% str_replace("\\{", "c(") %>%
                              str_replace("\\}", ") + 1"))
   code <- c(code, tmp %>% str_c(collapse = ", ") %>% str_c("cu_faces <- rbind(", ., ")"))
   tmp <- c()
-  for(i in 25:32) tmp <- c(tmp, data[i] %>% str_replace("\\{", "c(") %>%
+  for(i in 49:56) tmp <- c(tmp, data[i] %>% str_replace("\\{", "c(") %>%
                              str_replace("\\}", ") + 1"))
   code <- c(code, tmp %>% str_c(collapse = ", ") %>% str_c("oct_faces <- rbind(", ., ")"))
   eval(parse(text=code))
 
-  texts <- c(str_c('o', 1:4),c('t1','s1','t2','s2','s3','s4','t3','t4'))
+  texts <- c(str_c('p', 1:8), str_c('q', 1:8), str_c('r', 1:8))
 
   segs <- list()
   for(i in 1:nrow(cu_faces)) {
@@ -69,28 +68,28 @@ get_cuboctahedron <- function() {
   segix <- get_set(segx) %>% as.list() %>% unlist()
   segments <- verts[segix,]
 
-  cuboctahedron <- list(
-    info = c(14,24,12) %>% `names<-`(c('facces', 'edges', 'vertices')),
+  rhombicuboctahedron <- list(
+    info = c(26,48,24) %>% `names<-`(c('facces', 'edges', 'vertices')),
     verts = verts,
     texts = texts,
     segments = segments,
     cu_faces = cu_faces,
     oct_faces = oct_faces
   )
-  cuboctahedron
+  rhombicuboctahedron
 }
 
-#'  Cuboctahedron
-#' @details A cuboctahedron data structure.
+#'  Rhombicuboctahedron
+#' @details A rhombicuboctahedron data structure.
 #' \describe{
 #'  \item{verts}{The vertex coordinates}
 #'  \item{texts}{The vertex labels}
-#'  \item{segments}{The edges of the cuboctahedron}
+#'  \item{segments}{The edges of the rhombicuboctahedron}
 #'  \item{cu_faces}{The quadrilateral faces}
 #'  \item{oct_faces}{The triangular faces}
 #' }
 #'
-cuboctahedron <- get_cuboctahedron()
-save(cuboctahedron, file = "data/cuboctahedron.rda")
+rhombicuboctahedron <- get_rhombicuboctahedron()
+save(rhombicuboctahedron, file = "data/rhombicuboctahedron.rda")
 
 
